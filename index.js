d3ec5091e6f32a0d6954be0ec1b29356ee144f1b
koiclone <cheryljcruz1@gmail.com>
2022-04-29 22:10:47 -0500

    db connect, return prompt, and data selection parse

--- index.js
+++ index.js
@@ -10,27 +10,57 @@ const cTable = require("console.table");
 
 // start prompt
 const startPrompt = () => {
-  return inquirer.prompt([
-    {
-      type: "list",
-      name: "start",
-      message: "Hello! Please select an option!",
-      choices: [
-        "View all Departments",
-        "View all Roles",
-        "View all Employees",
-        "Add a Department",
-        "Add a Role",
-        "Add an Employee",
-        "Update Employee role",
-        "Exit",
-      ],
-    },
-  ]);
-  // parse selection
+  return (
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "start",
+          message: "Hello! Please select an option!",
+          choices: [
+            "View all Departments",
+            "View all Roles",
+            "View all Employees",
+            "Add a Department",
+            "Add a Role",
+            "Add an Employee",
+            "Update Employee role",
+            "Exit",
+          ],
+        },
+      ])
+      // parse selection
+      .then((data) => {
+        if (data.start === "View all Departments") {
+          getDepartment();
+        } else if (data.start === "View all Roles") {
+          getRole();
+        } else if (data.start === "View all Employees") {
+          getEmployee();
+        }
+      })
+  );
 };
 
 // return to selection
+const returnPrompt = () => {
+  return inquirer
+    .prompt([
+      {
+        type: "confirm",
+        name: "confirmReturn",
+        message: "Would you like to select another option?",
+        default: false,
+      },
+    ])
+    .then((returnData) => {
+      if (returnData.confirmReturn) {
+        startPrompt();
+      }
+      console.log("Goodbye!");
+      return;
+    });
+};
 
 // view employee
 getEmployee = (data) => {
@@ -41,6 +71,7 @@ getEmployee = (data) => {
     }
     console.table(result);
     //return to prompt
+    returnPrompt();
   });
 };
 
@@ -54,6 +85,7 @@ getRole = (data) => {
     console.table(result);
 
     // return to prompt
+    returnPrompt();
   });
 };
 
@@ -67,5 +99,15 @@ getDepartment = (data) => {
     console.table(result);
 
     // return to prompt
-  })
-}
+    returnPrompt();
+  });
+};
+
+// connect to db
+db.connect((err) => {
+  if (err) throw err;
+  console.log("Connected to DB");
+
+  // start prompt call
+  startPrompt();
+});
